4bad240b126b950b885d43c554ebdf1c6398477f
kk6418 <kk6418@DESKTOP-9MOO913.localdomain>
2020-02-08 21:23:32 +0000

    Vocabulary

--- src/vocabulary /vocabulary.java
+++ src/vocabulary /vocabulary.java
@@ -1,4 +1,6 @@
-public Class Vocabulary {
+import java.util.ArrayList;
+
+public class vocabulary {
 
     enum Levels {
         LEVEL1,
@@ -6,6 +8,23 @@ public Class Vocabulary {
         LEVEL3,
         LEVEL4,
         LEVEL5;
+
+    private static final int size = Levels.values().length;
+
+    Levels[] array = Levels.class.getEnumConstants();
+    
+    public ArrayList<ArrayList<String>> getVocabulary() {
+        
+        ArrayList<ArrayList<String>> vocabulary = new ArrayList<ArrayList<String>>();
+            vocabulary.add(Level1.values()[0].toList());
+            vocabulary.add(Level2.values()[0].toList());
+            vocabulary.add(Level3.values()[0].toList());
+            vocabulary.add(Level4.values()[0].toList());
+            vocabulary.add(Level5.values()[0].toList());
+
+        return vocabulary;
+    }
+
     }
 
     enum Level1 {
@@ -14,6 +33,20 @@ public Class Vocabulary {
         FATHER,
         SISTER,
         BROTHER;
+
+    private static final int size = Level1.values().length;
+
+    Level1[] array = Level1.class.getEnumConstants();
+
+    private ArrayList<String> toList() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(array[i].name());
+        }
+
+        return list;
+    }
+
     }
 
     enum Level2 {
@@ -22,6 +55,21 @@ public Class Vocabulary {
         GREEN,
         BLUE,
         YELLOW;
+    
+    private static final int size = Level2.values().length;
+
+    Level2[] array = Level2.class.getEnumConstants();
+
+    private ArrayList<String> toList() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(array[i].name());
+        }
+
+        return list;
+    }
+
+        
     }
 
     enum Level3 {
@@ -31,6 +79,19 @@ public Class Vocabulary {
         FISH,
         MOUSE;
 
+    private static final int size = Level3.values().length;
+
+    Level3[] array = Level3.class.getEnumConstants();
+
+    private ArrayList<String> toList() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(array[i].name());
+        }
+
+        return list;
+    }
+
     }
     enum Level4 {
         CHAIR,
@@ -38,6 +99,19 @@ public Class Vocabulary {
         SOFA,
         BED,
         LAMP;
+
+    private static final int size = Level4.values().length;
+
+    Level4[] array = Level4.class.getEnumConstants();
+
+    private ArrayList<String> toList() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(array[i].name());
+        }
+
+        return list;
+    }
         
     }
 
@@ -47,6 +121,19 @@ public Class Vocabulary {
         BANANA,
         CHERRY,
         ORANGE;
+
+    private static final int size = Level5.values().length;
+
+    Level5[] array = Level5.class.getEnumConstants();
+
+    private ArrayList<String> toList() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(array[i].name());
+        }
+
+        return list;
+    }
     }
 
 }
